3b5166ec7eaa8b0f832a2db0521686389ef815c1
Leo-yang0517 <57220587+Leo-yang0517@users.noreply.github.com>
2019-12-17 23:56:33 +0800

    新增数据分析，平均数函数原型，delete函数变为void型

--- fun.h
+++ fun.h
@@ -14,11 +14,12 @@ void init_list(void);
 Node* search(Node* list, char* id);
 Node* add_student(Node* list, char id);
 Student write(void);
-Node* delet(Node** list, char *id);
+void delet(Node** list, char *id);
 Node* update(Node* list, char *id);
 void print_one(Node* list);
 void print_all(Node* list);
 void modeset(int w, int h);
-
+void analyse(Node *list);
+void average(Node *list);
 #endif // _FUN_
 
